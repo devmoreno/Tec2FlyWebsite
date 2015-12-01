424096a4cc625fb93ea00e09a5544ebbb6a67c64
Edwin Moreno <devmoreno@Edwins-MacBook-Pro.local>
2015-12-01 18:08:47 -0400

    added pics

--- assets/js/app/Controllers/indexController.js
+++ assets/js/app/Controllers/indexController.js
@@ -14,6 +14,7 @@
 		 * Public Functions
 		 * */
 		$scope.sendRequest = function () {
+			//construct the json object
 			var data = {
 				"FirstName": $scope.FirstName,
 				"LastName":$scope.LastName,
